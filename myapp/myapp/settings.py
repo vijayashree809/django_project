51b1b089c282f6d5dec44cb28ff74e68647e83f2
Vijayashree Veerabathiran <vijayashreeveerabathiran@gmail.com>
2025-08-11 01:25:36 +0530

    Initial commit: Django project setup

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -124,6 +124,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     "/blog/static/blog/"
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
--- myapp/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def custom_page_not_found(request, exception):
-    return render(request, '404.html',status=404)
